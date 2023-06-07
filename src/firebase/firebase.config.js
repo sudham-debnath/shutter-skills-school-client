5ffcebfc6a3e7ce8ca92eba9d592f3cabd31341b
SudhamDebnath <sudham15-12854@diu.edu.bd>
2023-06-08 01:20:22 +0600

    Setup Firebase Environment

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCPF5QqytaMk7lxqsRPsQqfh6kgC3VYzSs",
+  authDomain: "shutter-skills-school-client.firebaseapp.com",
+  projectId: "shutter-skills-school-client",
+  storageBucket: "shutter-skills-school-client.appspot.com",
+  messagingSenderId: "322276476214",
+  appId: "1:322276476214:web:35896c30235f203e6a5f97"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
