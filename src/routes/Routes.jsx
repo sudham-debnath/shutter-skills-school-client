561824e08a96c077dea7793775454c6ca7a85a7a
SudhamDebnath <sudham15-12854@diu.edu.bd>
2023-06-08 20:24:39 +0600

    Setup All Routes

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-unused-vars
+import { createBrowserRouter } from "react-router-dom";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+import Home from "../pages/Home/Home";
+import Blogs from "./../pages/Blogs/Blogs";
+import ErrorPage from "./../pages/ErrorPage/ErrorPage";
+import App from "../App";
+import PrivateRoute from "./PrivateRoute";
+import AllToys from "../pages/AllToys/AllToys";
+import MyToys from "../pages/MyToys/MyToys";
+import AddToy from "../pages/AddToy/AddToy";
+import ToyDetails from "../pages/ToyDetails/ToyDetails";
+import EditToy from "../pages/EditToy/EditToy";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+      {
+        path: "register",
+        element: <Register></Register>,
+      },
+      {
+        path: "blogs",
+        element: <Blogs></Blogs>,
+      },
+      {
+        path: "myToys",
+        element: (
+          <PrivateRoute>
+            <MyToys></MyToys>
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+]);
+
+export default router;
