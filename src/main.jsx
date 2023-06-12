3c3d321744df6260a363080883218040a4c00ba8
SudhamDebnath <sudham15-12854@diu.edu.bd>
2023-06-12 21:20:39 +0600

    Setup Tanstack Query

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/Routes";
 import AuthProvider from "./provider/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider client={new QueryClient()}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- src/pages/Dashboard/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers.jsx
@@ -1,10 +1,17 @@
 /* eslint-disable no-unused-vars */
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const AllUsers = () => {
+
+    const { data: users = [], refetch } = useQuery(['users'], async () => {
+        const res = await fetch("http://localhost:5000/users")
+        return res.json();
+    })
+    
     return (
         <div>
-            All Users
+            {users.length}
         </div>
     );
 };
